ef711392329410d6cd1fc484b6dead1d93644eed
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-05-15 17:35:27 +0800

    fix: removed smoothing from player movement for more responsive controls

--- Assets/Scripts/Player/Controllers/Movement/RunMovement.cs
+++ Assets/Scripts/Player/Controllers/Movement/RunMovement.cs
@@ -11,8 +11,8 @@ public class RunMovement : IMovementStrategy
 
     public void Move(Rigidbody rb, Transform transform)
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
         Vector3 input = transform.forward * z + transform.right * x;
         Vector3 direction = input.normalized * _speed * Time.deltaTime;
