e20a520577a455d998d40b0654028498faeea973
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-06-29 21:37:24 +0800

    fix: disable door collider during opening animation
    - Temporarily disabled the door’s collider while the opening animation plays.
    - Prevents the door from pushing the player during entry.

--- Assets/Code/Game/Door/Doors/SingleHingedDoor.cs
+++ Assets/Code/Game/Door/Doors/SingleHingedDoor.cs
@@ -1,31 +1,38 @@
 using UnityEngine;
-
 public class SingleHingedDoor : DoorBase
 {
+    [SerializeField] private Collider _collider;
     [SerializeField] private float _openAngle = 90f;
     [SerializeField] private float _openSpeed = 10f;
 
     private Quaternion _closedRotation;
     private Quaternion _targetRotation;
-
     private void Start()
     {
         _type = DoorType.SingleHindedDoor;
+
         _closedRotation = transform.rotation;
         _targetRotation = _closedRotation;
-    }
 
+        _collider = GetComponent<Collider>();
+        if (_collider == null) Debug.Log("Collider - not found");
+    }
     public override void Interaction()
     {
         _isOpen = !_isOpen;
         float angle = _isOpen ? _openAngle : 0f;
+
         _targetRotation = Quaternion.Euler(0, angle, 0) * _closedRotation;
 
         SoundManager.Instance.PlayDoorSound(_type, _isOpen, _audioSource);
     }
-
     private void Update()
     {
-        transform.rotation = Quaternion.LerpUnclamped(transform.rotation, _targetRotation, Time.deltaTime * _openSpeed);
+        if (transform.rotation != _targetRotation)
+        {
+            transform.rotation = Quaternion.LerpUnclamped(transform.rotation, _targetRotation, Time.deltaTime * _openSpeed);
+            _collider.isTrigger = true;
+        }
+        else _collider.isTrigger = false;
     }
 }
